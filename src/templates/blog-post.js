6103618243a67c534b7d9d290fc5ec9f6a8273dc
JongGyun Park <jonggyuni@gmail.com>
2019-03-01 09:38:37 +0900

    adjust normalizecss

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import 'normalize.css';
+//import 'normalize.css';
 
 import Bio from '../components/Bio';
 import Layout from '../components/Layout';
